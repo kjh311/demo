c8a7264f759eb635d9aa5b664e704a30d94d3efb
Kevin Huelsmann <kjh311@gmail.com>
2021-02-16 08:01:17 -0800

    demo

---
+++ bodymovin3.js
@@ -0,0 +1,10 @@
+var animation = bodymovin.loadAnimation({
+	// container: document.getElementById('brand-logo-iframe'),
+	container: document.getElementById('nav-logo'),
+	renderer: 'svg',
+	loop: false,
+	autoplay: true,
+	path: 'https://images.shulcloud.com/14479/uploads/data.json'
+})
+
+// alert(animation)
